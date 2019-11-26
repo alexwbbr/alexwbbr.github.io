77b2dfa6e74719895971f41407e90ef61a34cc9f
Alex Webber <alexwbbr@gmail.com>
2019-11-26 20:29:48 +0000

    basic sticky header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'test';
+export class AppComponent implements OnInit {
+
+  public ngOnInit(): void {
+    setTimeout(() => {
+      document.body.getElementsByClassName('splash')[0].classList.add('hide');
+    }, 2000);
+  }
+
 }
